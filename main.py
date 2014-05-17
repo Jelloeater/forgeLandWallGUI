99731a201b773344a4a3d0f5f73ffd33eda32269
Jesse <jelloeater@gmail.com>
2014-05-17 17:49:38 -0400

    added listbox

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from Tkinter import *
+from sys import version_info
+
+try:
+	from tkinter import *
+except ImportError:
+	from Tkinter import *
 
 def Pressed():                          #function
-	print 'buttons are cool'
+	print ('buttons are cool')
 	print(v.get())
 	if v.get():
 		print("cat")
+		newWindow = Tk()
+		l = Label(newWindow,text="LOL")
+		l.pack()
+		list = Listbox(newWindow)
+		stuff=[1,2]
+		for x in stuff:
+			list.insert(0,x)
+		list.pack()
 
 root = Tk()                             #main window
 
@@ -17,5 +30,6 @@ button = Button(root, command = Pressed)
 button.pack()
 
 root.mainloop()
+
 
 # https://wiki.python.org/moin/Intro%20to%20programming%20with%20Python%20and%20Tkinter
