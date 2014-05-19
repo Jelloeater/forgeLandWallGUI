d49def0b04331f7386e7db77a86df55b95da4da1
Jesse <jelloeater@gmail.com>
2014-05-19 18:05:25 -0400

    Moved functions into classes

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 from Tkinter import *
 
-
 class AppUI(Frame):
-
-	def menuBar(self):
-		self.menubar = Menu(self)
-		menu = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="File", menu=menu)
-		menu.add_command(label="Quit", command=self.Exit)
-		menu = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="Edit", menu=menu)
-		menu.add_command(label="Cut")
-		menu.add_command(label="Copy")
-		menu.add_command(label="Paste")
-		self.master.config(menu=self.menubar)
-
-
-	def Exit(self):
-		root.destroy()
-
-
 	def __init__(self, master=None):
 		Frame.__init__(self, master, bd=2)
 
-		self.menuBar()
+		# self.menuBar()
+		menuBar(root)
+
 
 		# self.v = IntVar()
 		# c = Checkbutton(root, text="Don't show this again", variable=self.v)
@@ -32,28 +15,56 @@ class AppUI(Frame):
 		#
 		# button = Button(root, text = 'Press Me', command = self.Pressed)
 		# button.grid(column=2, sticky='WE')
+		autoRefresh(self)
+		quitButton(root)
+
 		quitButton(root)
 		quitButton(self)
+		quitButton(self)
 
-	# def Pressed(self):                          #function
-	# 	print 'buttons are cool'
-	# 	print(self.v.get())
-	# 	if self.v.get():
-	# 		print("cat")
-	# 		new = Tk()
-	# 		box = Canvas(new)
-	# 		box.grid()
+		# def Pressed(self):                          #function
+		# 	print 'buttons are cool'
+		# 	print(self.v.get())
+		# 	if self.v.get():
+		# 		print("cat")
+		# 		new = Tk()
+		# 		box = Canvas(new)
+		# 		box.grid()
+	@classmethod
+	def Exit(cls):
+		root.destroy()
+
+class autoRefresh(Checkbutton):
+	def __init__(self, parent):
+		Checkbutton.__init__(self, parent)
+		self['text'] = 'Refresh'
+		self.grid()
 
 
-# Create a class that specializing the Button class from the tkinter
 class quitButton(Button):
 	def __init__(self, parent):
 		Button.__init__(self, parent)
 		# Change the message here
 		self['text'] = 'Good Bye'
 		# Command to close the window (the destory method)
-		self['command'] = parent.destroy
-		self.grid(row=1,sticky='nsew')
+		self['command'] = root.destroy
+		self.grid(sticky='nsew')
+
+# Create a class that specializing the Button class from the tkinter
+class menuBar(Menu):
+	def __init__(self, parent, **kw):
+		Menu.__init__(self, **kw)
+		self.menubar = Menu()
+		fileMenu = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="File", menu=fileMenu)
+		fileMenu.add_command(label="Quit", command=AppUI.Exit)
+		editMenu = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="Edit", menu=editMenu)
+		editMenu.add_command(label="Cut")
+		editMenu.add_command(label="Copy")
+		editMenu.add_command(label="Paste")
+		self.master.config(menu=self.menubar)
+
 
 
 root = Tk()
