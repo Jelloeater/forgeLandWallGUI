3c195753ae84494d1a7730543d7f622ec86caa5d
Jesse <jelloeater@gmail.com>
2014-05-21 20:55:53 -0400

    Merge branch 'origin/model'
    Conflicts:
    	main.py

--- wallGui/__init__.py
+++ wallGui/__init__.py
@@ -1,1 +1,10 @@
 __author__ = 'Jesse'
+import json
+import urllib
+
+class messageList:
+	messageList = []
+
+	@classmethod
+	def getMessagesFromServer(cls,numberToGet):
+		pass
---
+++ wallGui/model.py
@@ -0,0 +1,7 @@
+__author__ = 'Jesse'
+
+class message:
+	def __init__(self):
+		self.message = None
+		self.timestamp = None
+		self.index = None
