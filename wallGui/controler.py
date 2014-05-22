7221bcccfd5273d772ffc5f0390832b64eca0a8d
Jesse <jelloeater@gmail.com>
2014-05-21 20:16:42 -0400

    Started from scratch with GUI

--- wallGui/controler.py
+++ wallGui/controler.py
@@ -1,10 +1,10 @@
 __author__ = 'Jesse'
-
+from settings import *
 
 class messageList(web):
 	def getMessagesFromServer(cls,numberToGet):
 		rawJson = urllib.urlopen(web.serverAddress+'get/'+str(numberToGet))
 		print(rawJson)
-from settings import *
 
-__author__ = 'Jesse'
+
+
