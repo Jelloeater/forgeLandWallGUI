0b5b35e4df95352ae6afce2735e39225cc604161
Jesse <jelloeater@gmail.com>
2014-05-22 00:31:20 -0400

    Cleaned out __init__

--- wallGui/__init__.py
+++ wallGui/__init__.py
@@ -1,10 +1,1 @@
 __author__ = 'Jesse'
-import json
-import urllib
-
-class messageList:
-	messageList = []
-
-	@classmethod
-	def getMessagesFromServer(cls,numberToGet):
-		pass
--- wallGui/main.py
+++ wallGui/main.py
@@ -3,8 +3,9 @@ import logging
 from controler import messageController
 
 logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)", level=logging.DEBUG)
+class main(messageController):
+	@classmethod
+	def main(cls):
+		logging.debug("Started main program")
 
-def main():
-	logging.debug("Started main program")
-
-main()
+main.main()
