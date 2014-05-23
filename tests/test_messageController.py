eb08f45ba00428f54c008b1b9b41bc989a6e6b80
Jesse <jelloeater@gmail.com>
2014-05-22 20:20:10 -0400

    YAY we have a GUI now

--- tests/test_messageController.py
+++ tests/test_messageController.py
@@ -6,6 +6,6 @@ __author__ = 'Jesse'
 class TestMessageController(TestCase):
 	def test_refreshMessageList(self):
 		""" Make sure we get a proper index (will fail on an empty table)"""
-		x = messageController.refreshMessageList(1)
+		x = messageController.refreshMessageList()
 		testVar = x[0]['index']
 		self.assertEqual(True, str.isdigit(str(testVar)), "No Number Found in [0]['index']")
--- wallGui/main.py
+++ wallGui/main.py
@@ -1,12 +1,35 @@
-import Tkinter
+from Tkinter import *
 import logging
 from controler import messageController
 
-class main(messageController):
+class menuBar(Menu):
+	def __init__(self, parent, **kw):
+		Menu.__init__(self, **kw)
+		self.menubar = Menu()
+		fileMenu = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="File", menu=fileMenu)
+		fileMenu.add_command(label="Quit", command = parent.destroy)
+		editMenu = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="Help", menu=editMenu)
+		editMenu.add_command(label="About")
+		self.master.config(menu=self.menubar)
+
+class main(messageController):	
 	@classmethod
 	def main(cls):
 		logging.debug("Started main program")
-		cls.refreshMessageList(2)
+		cls.refreshMessageList()
+		root = Tk()
+		root.columnconfigure(0, weight=1)
+		root.rowconfigure(0, weight=1)
+
+		menuBar(root)
+
+		root.grid()
+		root.mainloop()
+
 		logging.debug("EOP")
+
+
 
 main.main()
