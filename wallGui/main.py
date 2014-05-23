83cbb6489a7f5b0c3798d84c79802756498bb81b
Jesse <jelloeater@gmail.com>
2014-05-22 22:03:36 -0400

    Added menu bar and refreshList Button

--- wallGui/main.py
+++ wallGui/main.py
@@ -2,19 +2,7 @@ from Tkinter import *
 import logging
 from controler import messageController
 
-class menuBar(Menu):
-	def __init__(self, parent, **kw):
-		Menu.__init__(self, **kw)
-		self.menubar = Menu()
-		fileMenu = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="File", menu=fileMenu)
-		fileMenu.add_command(label="Quit", command = parent.destroy)
-		editMenu = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="Help", menu=editMenu)
-		editMenu.add_command(label="About")
-		self.master.config(menu=self.menubar)
-
-class main(messageController):	
+class main(messageController):
 	@classmethod
 	def main(cls):
 		logging.debug("Started main program")
@@ -24,6 +12,11 @@ class main(messageController):
 		root.rowconfigure(0, weight=1)
 
 		menuBar(root)
+		refreshList(root)
+
+
+
+
 
 		root.grid()
 		root.mainloop()
@@ -31,5 +24,24 @@ class main(messageController):
 		logging.debug("EOP")
 
 
+class menuBar(Menu):
+	def __init__(self, parent, **kw):
+		Menu.__init__(self, parent)
+		self.menubar = Menu()
+		fileMenu = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="File", menu=fileMenu, underline=1)
+		fileMenu.add_command(label="Quit", command=parent.destroy, underline=1)
+		editMenu = Menu(self.menubar, tearoff=0)
+		self.menubar.add_cascade(label="Help", menu=editMenu)
+		editMenu.add_command(label="About")
+		self.master.config(menu=self.menubar)
+
+class refreshList(Button):
+	def __init__(self, parent):
+		Button.__init__(self, parent)
+		self['text'] = 'Refresh List (F5)'
+		self['command'] = main.refreshMessageList
+		# self['accelerator'] = "F5"
+		self.grid(sticky='nsew')
 
 main.main()
