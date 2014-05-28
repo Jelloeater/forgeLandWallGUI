fc38a282559736a0855d1da8bbc68be97da64f86
Jesse <jelloeater@gmail.com>
2014-05-27 22:26:07 -0400

    Moved files around so the user can just run the program directly

---
+++ wallGui/setup.py
@@ -0,0 +1,17 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+	name='forgeLandWallGUI',
+	version='1.6',
+	packages=['', 'build.lib'],
+	package_dir={'forgeLandWallGui': ''},
+	url='',
+	license='',
+	author='Jesse',
+	author_email='',
+	description='',
+	install_requires=[''],
+)
